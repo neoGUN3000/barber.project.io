ae0034c72fd9a616c8c8a5f581e3e68f2c9f989b
neoGUN3000 <oluksijsergiiovic@gmail.com>
2024-08-08 22:51:15 -0500

    js scroll and responsive booking page

--- js/js.js
+++ js/js.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     window.location.hash = '#BOOKING'
-} )
+} );
+document.addEventListener('DOMContentLoaded', () => {
+    const targetScroll = document.querySelector('.contact-wraper');
+    const goDownBtn = document.getElementById('goDown');
+    const label = document.getElementsByName('label-for-btnGoes');
+   
+    goDownBtn.addEventListener('click', () => {
+        
+        if(goDownBtn.checked){
+            label[0].classList.add('btnGoesDown');
+            targetScroll.scrollIntoView({behavior: 'smooth'});
+            }else{
+                label[0].classList.remove('btnGoesDown');
+                goDownBtn.scrollIntoView({behavior: 'smooth'});
+            }
+           
+    })
+      
+
+    } )
+  
+
+
